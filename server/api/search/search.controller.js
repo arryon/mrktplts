537aa7b69d0aa2b793a4c80e24957e0c93b6c142
Arryon Tijsma <arryon@gmail.com>
2015-08-01 01:04:00 +0200

    more workings

--- server/api/search/search.controller.js
+++ server/api/search/search.controller.js
@@ -9,30 +9,32 @@ var request = Promise.promisify(require('request'));
 Promise.promisifyAll(request);
 
 var adsFromHtml = function (html) {
-  console.info(html);
   var $ = ch.load(html);
   var results = [];
   var allAds = $('article.search-result').not('.listing-cas').each(function (i, ad) {
     ad = $(ad);
     var img = $(ad.find('figure img')[0]);
-    results.push({
-      url: $(ad.find('.heading a')[0]).attr('href'),
-      img: 'http:'+img.attr('src'),
-      title: img.attr('title'),
-      price: $(ad.find('.price-new')[0]).text()
-    });
+    var src = img.attr('src');
+    var price = $(ad.find('.price-new')[0]).text();
+    if (price !== '') {
+      results.push({
+        url: $(ad.find('.heading a')[0]).attr('href'),
+        img: src.indexOf('http:') === 0 ? src : 'http:'+src,
+        title: img.attr('title'),
+        price: price
+      });
+    }
   });
   return results;
 }
 
-// Get list of searchs
 exports.index = function(req, res) {
-  var categories = req.param('categories');
+  var category = req.param('category');
   var zipcode = req.param('zipcode');
   var term = req.param('term');
   var distance = req.param('distance') || 0;
-  var priceFrom = req.param('from') || "0,00";
-  var priceTo = req.param('to') || "0,00";
+  var priceFrom = req.param('priceFrom') || "0,00";
+  var priceTo = req.param('priceTo') || "0,00";
   var opts = {
     query: term,
     postcode: zipcode,
@@ -40,21 +42,16 @@ exports.index = function(req, res) {
     sortBy: "SortIndex",
     sortOrder: "decreasing", //newest first
     priceFrom: priceFrom,
-    priceTo: priceTo
+    priceTo: priceTo,
+    categoryId: category,
+    numberOfResultsPerPage: "100"
   }
 
-  Promise.map(categories, function (cat) {
-    opts.categoryId = cat;
-    //in each category, find the results from marktplaats
-    var url = config.marktplaats + '/z.html?' + querystring.stringify(opts);
-    console.info(url);
-    return request(url)
-    .then(function (resp) {
-      return adsFromHtml(resp[0].body);
-    });
-  })
-  .then(function (results) {
-    console.info(results);
-    return res.json(results);
+  //in each category, find the results from marktplaats
+  var url = config.marktplaats + '/z.html?' + querystring.stringify(opts);
+  console.info(url);
+  request(url)
+  .then(function (resp) {
+    return res.json(adsFromHtml(resp[0].body));
   });
 };
